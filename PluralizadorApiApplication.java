b6fd7687842f77a48171379a1367eed21263a6a1
ClaudioSSA <63317322+ClaudioSSA@users.noreply.github.com>
2024-08-29 17:27:18 -0400

    Add files via upload
    Requisitos
    Java 11+
    Maven 3.6+
    Spring Boot 2.5+
    Instalación y Ejecución
    Clona el repositorio o descomprime el archivo :
    git clone https://github.com/tu-usuario/PluralizadorAPI.git
    Navega al directorio del proyecto:
    cd PluralizadorAPI
    Construye el proyecto con Maven:
    mvn clean install
    Ejecuta la aplicación:
    mvn spring-boot:run
    Realiza las pruebas necesarias con el cliente Postman a través de una solicitud POST a la dirección http://localhost:8080/api/pluralizar

---
+++ PluralizadorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PluralizadorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralizadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralizadorApiApplication.class, args);
+	}
+
+}
